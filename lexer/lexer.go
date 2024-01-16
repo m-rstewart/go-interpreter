3ea8ce80a4951630d5264d21e3b74d1ae1b517b0
m-rstewart <mikestewart90@gmail.com>
2024-01-15 18:11:37 -0800

    Add clarifying comments to lexer struct

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,9 +2,9 @@ package lexer
 
 type Lexer struct {
 	input        string
-	position     int
-	readPosition int
-	ch           byte
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
 }
 
 func New(input string) *Lexer {
