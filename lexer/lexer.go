70f2d7ce8ede1fbe34d8c2060ee85d46306dc777
m-rstewart <mikestewart90@gmail.com>
2024-01-15 18:10:57 -0800

    Add lexer type and New function

---
+++ lexer/lexer.go
@@ -0,0 +1,15 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	lex := &Lexer{
+		input: input,
+	}
+	return lex
+}
