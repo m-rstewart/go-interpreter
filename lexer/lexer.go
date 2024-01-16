0181d93c2939deebf5e26217b790e4e28b0ce3fe
m-rstewart <mikestewart90@gmail.com>
2024-01-15 18:26:45 -0800

    Add NextToken method to lexer to create tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/m-rstewart/go-interpreter/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -11,6 +13,7 @@ func New(input string) *Lexer {
 	lex := &Lexer{
 		input: input,
 	}
+	lex.readChar()
 	return lex
 }
 
@@ -25,4 +28,39 @@ func (lex *Lexer) readChar() {
 	}
 	lex.position = lex.readPosition
 	lex.readPosition += 1
+}
+
+func (lex *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch lex.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, lex.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, lex.ch)
+	case '(':
+		tok = newToken(token.LPAREN, lex.ch)
+	case ')':
+		tok = newToken(token.RPAREN, lex.ch)
+	case ',':
+		tok = newToken(token.COMMA, lex.ch)
+	case '+':
+		tok = newToken(token.PLUS, lex.ch)
+	case '{':
+		tok = newToken(token.LBRACE, lex.ch)
+	case '}':
+		tok = newToken(token.RBRACE, lex.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	lex.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tokenType,
+		Literal: string(ch),
+	}
 }
