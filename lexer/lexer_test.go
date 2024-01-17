06f46ebb2dd592a1932c5511dfa154eb13f4f314
m-rstewart <mikestewart90@gmail.com>
2024-01-16 20:55:52 -0800

    Extend tests for two-character tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,6 +21,9 @@ func TestNextTokenSimple(t *testing.T) {
 	} else {
 			return false;
 	}
+
+	10 == 10; 
+	10 != 9;
 `
 
 	tests := []struct {
@@ -92,6 +95,14 @@ func TestNextTokenSimple(t *testing.T) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
